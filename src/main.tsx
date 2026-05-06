6230146c2731e1ed9a8ec413eec93baf93bfe031
Steven Dias <stevendias11@gmail.com>
2026-05-06 16:48:37 -0400

    wire up posthog analytics for nynjworldcup project

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import posthog from 'posthog-js';
 import { LandingPage } from '@/landing/LandingPage';
 import './styles.css';
+
+posthog.init('phc_CQ9UJEmmzhe586scbjKrxUtRD3yy8obAhiYdtwZZtkFA', {
+  api_host: 'https://us.i.posthog.com',
+  defaults: '2025-05-24',
+  person_profiles: 'identified_only',
+});
 
 const rootEl = document.getElementById('root');
 if (!rootEl) throw new Error('Missing #root in index.html');
